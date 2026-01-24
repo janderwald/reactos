48cfb59315b4c245d615f310ce8233e56b938ca3
Johannes Anderwald <johannes.anderwald@reactos.org>
2026-01-24 15:47:16 +0100

    [HDAUDIO] - Parsing improvements

--- drivers/wdm/audio/drivers/hdaudio/private.h
+++ drivers/wdm/audio/drivers/hdaudio/private.h
@@ -62,7 +62,7 @@ typedef struct
 
 typedef struct
 {
-    UCHAR MuteCapable : 1; 
+    UCHAR MuteCapable : 1;
     UCHAR Steps : 7;
     UCHAR NumSteps : 7;
     UCHAR Offset : 7;
@@ -148,6 +148,7 @@ class CAdapterCommon : public CUnknownImpl<IAdapterPowerManagement>
         IN ULONG AssociatedPinCount,
         IN PULONG AssociatedPins,
         IN PVOID Node,
+        IN UCHAR Digital,
         OUT PPCFILTER_DESCRIPTOR *OutDescription);
     NTSTATUS NTAPI
     BuildWaveInFilter(
@@ -165,8 +166,7 @@ class CAdapterCommon : public CUnknownImpl<IAdapterPowerManagement>
         IN PVOID OutNode,
         IN ULONG AssociatedPinCount,
         IN PULONG AssociatedPins,
-        IN ULONG PinNodeCount,
-        IN PULONG Pins);
+        IN UCHAR Digital);
     VOID NTAPI ClearRef(IN ULONG RefValue, IN ULONG NodeCount, IN PULONG Nodes);
     NTSTATUS
     NTAPI
@@ -261,7 +261,7 @@ class CFunctionGroupNode
   public:
     CFunctionGroupNode(UCHAR CodecAddress, UCHAR NodeId, CAdapterCommon *AdapterCommon)
         : m_Adapter(AdapterCommon), m_StartNodeId(NodeId), m_CodecAddress(CodecAddress), m_SubNodeCount(0)
-          
+
     {
         InitializeListHead(&m_Nodes);
     }
@@ -332,6 +332,15 @@ class CMiniportWaveRTStream : public CUnknownImpl<IMiniportWaveRTStreamNotificat
     }
     virtual ~CMiniportWaveRTStream()
     {
+        NTSTATUS Status;
+        HDAUDIO_BUS_INTERFACE_V2 Interface;
+
+        Status = m_Adapter->GetInterface(&Interface);
+        if (NT_SUCCESS(Status))
+        {
+            Interface.FreeDmaEngine(Interface.Context, m_DmaEngine);
+            return;
+        }
     }
 
     IMP_IMiniportWaveRTStreamNotification;
