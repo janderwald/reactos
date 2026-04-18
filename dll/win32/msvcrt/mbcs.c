97124e756149c2d2a0b9b05b31102882c6300e39
Johannes Anderwald <johannes.anderwald@reactos.org>
2026-04-18 14:37:26 +0200

    [MVSVCRT] - Fix crash of foobar2000 and others

--- dll/win32/msvcrt/mbcs.c
+++ dll/win32/msvcrt/mbcs.c
@@ -264,6 +264,7 @@ threadmbcinfo* create_mbcinfo(int cp, LCID lcid, threadmbcinfo *old_mbcinfo)
   if(lcid == -1) {
     WCHAR wbuf[LOCALE_NAME_MAX_LENGTH];
     sprintf(bufA, ".%d", newcp);
+    wbuf[0] = L'\0';
     mbcinfo->mblcid = locale_to_sname(bufA, NULL, NULL, wbuf) ? LocaleNameToLCID(wbuf, LOCALE_ALLOW_NEUTRAL_NAMES) : -1;
   } else {
     mbcinfo->mblcid = lcid;
